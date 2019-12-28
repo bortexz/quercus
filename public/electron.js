1fc082328032778f864b0f3c4a0ea74e46fb5038
rafi993 <rafi993@protonmail.com>
2019-12-28 12:34:31 +0530

    Adding dev tools

--- public/electron.js
+++ public/electron.js
@@ -3,6 +3,12 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const isDev = require('electron-is-dev');
+const {
+  default: installExtension,
+  REACT_DEVELOPER_TOOLS,
+  REDUX_DEVTOOLS,
+} = require('electron-devtools-installer');
+
 let mainWindow;
 function createWindow() {
   mainWindow = new BrowserWindow({ width: 900, height: 680 });
@@ -12,6 +18,16 @@ function createWindow() {
       : `file://${path.join(__dirname, '../build/index.html')}`
   );
   mainWindow.on('closed', () => (mainWindow = null));
+
+  if (isDev) {
+    installExtension(REDUX_DEVTOOLS)
+      .then(name => console.log(`Added Extension:  ${name}`))
+      .catch(err => console.log('An error occurred: ', err));
+
+    installExtension(REACT_DEVELOPER_TOOLS)
+      .then(name => console.log(`Added Extension:  ${name}`))
+      .catch(err => console.log('An error occurred: ', err));
+  }
 }
 
 app.on('ready', createWindow);
