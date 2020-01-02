489ec36f41b6e205ad5f91e2bbd7864e28a2c3b3
rafi993 <rafi993@protonmail.com>
2020-01-02 08:40:07 +0530

    Truncating file names in two lines

--- src/components/GridView/styles.ts
+++ src/components/GridView/styles.ts
@@ -9,21 +9,25 @@ export const Item = styled.div`
   display: grid;
   justify-content: center;
   align-items: center;
-  height: 85px;
-  width: 85px;
+  height: 90px;
+  width: 90px;
   > svg {
     height: 40px;
-    margin: 10px auto;
     width: 40px;
+    margin: 10px auto;
+    margin-bottom: 0;
   }
 
   > h4 {
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
     overflow: hidden;
-    white-space: nowrap;
-    text-overflow: ellipsis;
     font-size: 0.8rem;
     margin: 0;
     text-align: center;
     width: 74px;
+    word-break: break-all;
+    height: 1.6rem;
   }
 `;
