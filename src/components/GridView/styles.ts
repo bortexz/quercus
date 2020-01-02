9f1967f9279446d6a2c3a86b8ae818de8896c961
rafi993 <rafi993@protonmail.com>
2020-01-02 08:12:26 +0530

    Alignment of grid view

--- src/components/GridView/styles.ts
+++ src/components/GridView/styles.ts
@@ -2,28 +2,28 @@ import styled from 'styled-components';
 
 export const GridView = styled.div`
   padding: 1.5em 2em;
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(8em, 1fr));
-  grid-gap: 10px;
 `;
 
 export const Item = styled.div`
+  float: left;
   display: grid;
   justify-content: center;
   align-items: center;
-  height: 5em;
+  height: 85px;
+  width: 85px;
   > svg {
-    height: 3em;
-    width: 3em;
+    height: 40px;
+    margin: 10px auto;
+    width: 40px;
   }
 
   > h4 {
-    width: 5em;
     overflow: hidden;
     white-space: nowrap;
     text-overflow: ellipsis;
     font-size: 0.8rem;
     margin: 0;
-    text-align: left;
+    text-align: center;
+    width: 74px;
   }
 `;
